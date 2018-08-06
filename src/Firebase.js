faa5a2b3b7117c9945f8cef94b1937422827ca9f
Edward <rumatha@gmail.com>
2018-08-06 12:13:28 +0300

    template for storage config

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,7 @@ require("firebase/auth");
 require("firebase/database");
 
 var config = {
-    apiKey: "AIzaSyDLlXJjbB99bWMaUyWq5o-t_kzAxobcbaA",
-    authDomain: "multicam-tally.firebaseapp.com",
-    databaseURL: "https://multicam-tally.firebaseio.com",
-    projectId: "multicam-tally",
-    storageBucket: "multicam-tally.appspot.com",
-    messagingSenderId: "682322906139"
+
 };
 
 firebase.initializeApp(config);
